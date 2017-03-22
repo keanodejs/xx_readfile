028ea9ffe9e0eac258814928c867b7f4a58f87cf
Claus Bové <clbo@kea.dk>
2017-03-22 10:40:07 +0100

    Update readfile.js

--- readfile.js
+++ readfile.js
@@ -1,10 +1,5 @@
 var fs = require('fs');
 
-// syncronious version
-// var x = fs.readFileSync('index.html', 'utf-8');
-// console.log(x);
-
-// assync version
 fs.readFile('index.html', 'utf-8', function(err, data){
 	console.log(data);
 });
